a5a2a26c64cc0ad81e0b373612b3a05c3092db65
Szymon-Budziak <szymonbudziak@gmail.com>
2024-08-29 19:40:29 +0200

    chore: generator model refactor

--- src/generator/generator.py
+++ src/generator/generator.py
@@ -1,5 +1,5 @@
 from tensorflow.keras.models import Model, Sequential
-from tensorflow.keras.layers import BatchNormalization, Conv2D, Dense, Input, Reshape, UpSampling2D
+from tensorflow.keras.layers import BatchNormalization, Conv2D, Dense, Input, Reshape, UpSampling2D, Activation
 
 __all__ = ["build_generator"]
 
@@ -12,38 +12,42 @@ def build_generator() -> Model:
         Model: The generator model
     """
     # Instantiate sequential model
-    generator = Sequential()
+    generator_model = Sequential()
 
     # Dense layer with input dimension 128 x 7 x 7
-    generator.add(Dense(units=128 * 7 * 7, activation="relu", input_dim=100))
+    generator_model.add(Dense(units=128 * 7 * 7, input_dim=100))
+    generator_model.add(Activation("relu"))
 
     # Reshape the image dimensions to 7 x 7 x 128
-    generator.add(Reshape(target_shape=(7, 7, 128)))
+    generator_model.add(Reshape(target_shape=(7, 7, 128)))
 
     # Up sample the image to 14 x 14
-    generator.add(UpSampling2D())
+    generator_model.add(UpSampling2D())
 
     # First Convolutional layer with Batch Normalization and image up sampling to 28 x 28
-    generator.add(Conv2D(filters=128, kernel_size=3, padding="same", activation="relu"))
-    generator.add(BatchNormalization(momentum=0.8))
-    generator.add(UpSampling2D())
+    generator_model.add(Conv2D(filters=128, kernel_size=3, padding="same"))
+    generator_model.add(BatchNormalization(momentum=0.8))
+    generator_model.add(Activation("relu"))
+    generator_model.add(UpSampling2D())
 
     # Second Convolutional layer with Batch Normalization, no up sampling because the image
     # is already 28 x 28 - equal to MNIST dataset image size
-    generator.add(Conv2D(filters=64, kernel_size=3, padding="same", activation="relu"))
-    generator.add(BatchNormalization(momentum=0.8))
+    generator_model.add(Conv2D(filters=64, kernel_size=3, padding="same"))
+    generator_model.add(BatchNormalization(momentum=0.8))
+    generator_model.add(Activation("relu"))
 
     # Third Convolutional layer with number of filters=1
-    generator.add(Conv2D(filters=1, kernel_size=3, padding="same", activation="relu"))
+    generator_model.add(Conv2D(filters=1, kernel_size=3, padding="same"))
+    generator_model.add(Activation("tanh"))
 
     # Print the model summary
-    generator.summary()
+    generator_model.summary()
 
     # Input noise of length 100, 100 chosen to create a simple network
     noise = Input(shape=(100,))
 
     # Run the generator to create the fake image
-    fake_image = generator(noise)
+    fake_image = generator_model(noise)
 
     # Return a model that takes as input the noise and outputs the fake image
-    return Model(inputs=noise, outputs=fake_image)
+    return Model(noise, fake_image)
